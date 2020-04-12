081de5b6391b069eea41e31c327e5d45fef8a60a
CharlieShatihoko <tsubaty0926@yahoo.co.jp>
2020-04-12 12:02:59 +0900

    url typo miss

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABESE_URL || 'postgres://postgres:postgres@localhost/swimrecord',
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/swimrecord',
   {
     operatorsAliases: false
   });
